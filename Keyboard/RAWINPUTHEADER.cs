dac9d86f7f649ff953ccdc1c2dfa2603a20c9a2c
Synless <Synless@users.noreply.github.com>
2023-05-09 10:46:23 +0200

    Add files via upload

---
+++ Keyboard/RAWINPUTHEADER.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace KeyboardLayoutSwapper.Keyboard
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public class RAWINPUTHEADER
+    {
+        public uint dwType;
+        public uint dwSize;
+        public IntPtr hDevice;
+        public IntPtr wParam;
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using KeyboardLayoutSwapper.Keyboard;
+using System.Windows;
+
+namespace KeyboardLayoutSwapper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Hide();
+            InitializeComponent();
+            Hook.Start();
+        }
+    }
+}
